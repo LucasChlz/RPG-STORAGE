2ab3c250ced96e9223566586ccfeca21fa612657
Lucas Gabriel <lucasgabrielsampaio2019@gmail.com>
2022-05-25 19:18:35 -0300

    installing express session for login and logout

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 require('dotenv').config();
 const express = require('express');
 const bodyParser = require('body-parser');
+const session = require('express-session');
 const app = express();
-const port = process.env.PORT
+const port = process.env.PORT;
+const secret = process.env.SECRET;
 
 const CreateUser = require('./Routes/Create.User.Route');
 
+app.use(session({ secret: secret }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(bodyParser.json({ type: 'application/*+json' }));
