5b07cc67dbc758f6ac7ec5ec8ec0814f0e75ee07
Lucas Gabriel <lucasgabrielsampaio2019@gmail.com>
2022-05-25 15:44:55 -0300

    adding login

--- Controllers/User.Controller.js
+++ Controllers/User.Controller.js
@@ -50,4 +50,34 @@ module.exports = {
             return res.send({name,email});
         }
     },
+
+    login: async (req, res) => {
+        const { email, password} = req.body;
+
+        const findUser = await UserModel.findOne({email: email});
+
+        if (findUser)
+        {
+            const passwordCompare = await bcrypt.compareSync(password, findUser.password);
+
+            if(passwordCompare)
+            {
+                var sess = req.session;
+                sess.name = findUser.name;
+                sess.email = findUser.email;
+                sess.login = true;
+
+                return res.send({
+                    name: sess.name,
+                    email: sess.email,
+                    login: sess.login
+                });
+            } else
+            {
+                return res.send({ message: "invalid password" });   
+            }
+        } else {
+            return res.send({ message: "email not found" });
+        }
+    },
 }
